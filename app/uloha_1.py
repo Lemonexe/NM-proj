797d2f4afb8b637f3e654855a75777598e97ae31
Jiri Zbytovsky <zbytek@gmail.com>
2023-01-27 14:43:49 +0100

    uloha_1 final ivp solution

--- app/uloha_1.py
+++ app/uloha_1.py
@@ -23,21 +23,17 @@ def solve_problem():
     # vektor derivací ve formě použitelné pro scipy funkci solve_ivp
     du = lambda x, u: (dy1(x, *u[0:2]), dy2(x, *u[0:2]), dp1(x, *u), dp2(x, *u))
 
+    # rozsah nezávislé veličiny x; nelze počítat přesně od nuly (neurčitý výraz dy2)
+    x_span = (1e-6, 1)
+
     # cyklus pro opakovaný výpočet eta, dokud není splněno kritérium konvergence
     while abs(eta - eta_prev) > eps:
         if k > max_iter:
             raise Exception(f'Konvergence nedosažena při {k} iteracích')
             break
 
-        # počáteční podmínky - hodnoty y1(0), y2(0), p1(0), p2(0)
-        y1_i = eta
-        y2_i = 0
-        p1_i = 1
-        p2_i = 0
-        u_i = (y1_i, y2_i, p1_i, p2_i)
-
-        # rozsah nezávislé veličiny x; nelze počítat přesně od nuly (neurčitý výraz dy2)
-        x_span = (1e-6, 1)
+        # získat vektor počátečních podmínek, viz deklarace get_u_i
+        u_i = get_u_i(eta)
 
         # získat řešení pro dané eta
         sol = solve_ivp(du, x_span, u_i, method='RK45', max_step=1e-3)
@@ -54,9 +50,34 @@ def solve_problem():
         k += 1
         eta_prev = eta
         eta = eta - theta / dtheta
-        print('k = {:.0f}; eta = {:6.3f}; theta = {:6.3f}'.format(k, eta, theta))
+        print('k = {:.0f};  eta = {:7.4f};  theta = {:7.4f}'.format(k, eta, theta))
 
-    print('Dosažena konvergence')
+    print('Dosažena konvergence, zahájeno poslední řešení')
+
+    # získat řešení pro konečnou hodnotu eta
+    # závislé proměnné sady diferenciálních rovnic jsou nyní pouze: y1, y2
+    # dohromady tvoří vektor u = (y1, y2)
+
+    du = lambda x, u: (dy1(x, *u), dy2(x, *u))
+    u_i = get_u_i(eta)[0:2]
+    sol = solve_ivp(du, x_span, u_i, method='RK45', max_step=1e-3)
+    x = sol.t
+    u = sol.y
+    y1 = u[0, :]
+    y2 = u[1, :]
+    y1_e, y2_e = u[:, -1]
+    print('Okrajové hodnoty výsledného řešení:')
+    print('y1(1) = {:7.4f};  y2(1) = {:7.4f}'.format(y1_e, y2_e))
+    print('Pro úplné řešení viz graf')
+
+
+# pro dané eta vygenerovat vektor hodnot počátečních podmínek u = y1(0), y2(0), p1(0), p2(0)
+def get_u_i(eta):
+    y1_i = eta
+    y2_i = 0
+    p1_i = 1
+    p2_i = 0
+    return (y1_i, y2_i, p1_i, p2_i)
 
 
 # spustit program

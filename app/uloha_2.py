60186f9dc2e256aca0d76b8a19bac715c6907a4a
Jiri Zbytovsky <zbytek@gmail.com>
2023-01-29 15:00:22 +0100

    uloha_2 commentary

--- app/uloha_2.py
+++ app/uloha_2.py
@@ -16,34 +16,50 @@ def solve_problem():
     x_i = np.linspace(0, 1, num=(n + 1))
     t_j = np.linspace(0, 1, num=(m + 1))
 
+    # poznámka k číslování: x_i má n+1 bodů, první je x_i[0], poslední je x_i[n]
+
     # počáteční podmínka - u v bodech x_i, t = 0
     u_i0 = 4 * (x_i - .5)**2
 
+    # matice bodů řešení, kde u[j, i] = u(x_i, t_j)
     u_final = np.zeros((m + 1, n + 1))
-    u_final[0, :] = u_i0
+    u_final[0, :] = u_i0  # první řádek (j=0) známe
 
-    C = k / h / h
-    D = lambda u_ij: 1 + C + u_ij
+    C = k / h / h  # alpha = k/h^2, pro snazší zápis zvoleno C
+    D = lambda u: 1 + C + u  # prostřední člen pásu matice (je závislý na u[j, i] z předchozího řádku u)
     u_ij = u_i0
 
+    # je třeba získat nový řádek u[j+1, :], přičemž jeho krajní body, čili i=0, i=n jsou přímo vyjádřeny okrajovou podmínkou
+    # prostřední body, odpovídající i=1 až i=(n-1) včetně, jsou řešením matice soustavy A · u_new = b, matice je tedy tvaru (n-1)·(n-1)
+    # důsledkem je, že číslování matice A a vektoru b se liší od číslování u,x,t; např. druhý člen u_new, což je u[j+1, 1], odpovídá pozici A[k, 0] pro každou rovnici k
+    # přičemž rovnice k je vyjádřením PDR pro bod x_(k+1), t_(j+1)
+    # cenou za linearizaci problému je to, že se matice A mění každou iteraci j (člen D závislý na u[j, :])
     A = np.zeros((n - 1, n - 1), dtype='float64')
-    for i in range(2, n - 1):
-        k = i - 1
-        A[k, (k - 1):(k + 2)] = [-C / 2, D(u_ij[i]), -C / 2]
+
+    # první a poslední řádek matice A, pro i=1, i=(n-1)
     A[0, 0:2] = [D(u_ij[1]), -C / 2]
     A[-1, -2:] = [-C / 2, D(u_ij[-2])]
 
+    # ostatní řádky matice A pro i = 2 až i=(n-2) včetně
+    for i in range(2, n - 1):
+        k = i - 1
+        A[k, (k - 1):(k + 2)] = [-C / 2, D(u_ij[i]), -C / 2]
+
+    # vektor pravých stran b je tvaru (n-1)·1, přičemž k je opět čítač PDR
     b = np.zeros((n - 1, 1), dtype='float64')
+    # všechny řádky b obsahují tento výraz...
     for i in range(1, n):
         k = i - 1
         b[k] = C / 2 * (u_ij[i - 1] - 2 * u_ij[i] + u_ij[i + 1]) + u_ij[i]
+
+    # ... kk prvním a posledním řádkům je ještě přičten tento výraz (vyjadřující známé hodnoty pro i=0, i=n)
     b[(0, -1), 0] += C / 2
 
+    # nyní je vyřešena soustava rovnic a sestaven nový řádek u[j+1, :]
     u_new = solve(A, b)
 
-    # print(np.concatenate((A,u_new,b), axis=1))
-    u_final[1, (0, -1)] = 1
-    u_final[1, 1:-1] = u_new.T
+    u_final[1, (0, -1)] = 1  # okrajové hondoty známe z okrajové podmínky
+    u_final[1, 1:-1] = u_new.T  # prostřední hodnoty z řešení soustavy rovnic
     print(u_final)
 
 
